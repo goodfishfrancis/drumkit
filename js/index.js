0c90e2d68502c57db5ab8039c9b9836cfb20bb49
Alexander Solinger <alexandersolinger@me.com>
2020-02-14 14:54:19 -0800

    added files for project

---
+++ js/index.js
@@ -0,0 +1,58 @@
+ // this detects a button press and plays the relevant sound
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        makeSound(this.innerHTML); 
+        buttonAnimation(this.innerHTML);
+    });
+}
+
+// this detects a keyboard press and plays the relevant sound
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);  
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function buttonAnimation(key) {
+   var activeButton = document.querySelector("." + key);
+   activeButton.classList.add("pressed");
+   setTimeout(function (){
+    activeButton.classList.toggle("pressed");
+   }, 100);
+   
+}
